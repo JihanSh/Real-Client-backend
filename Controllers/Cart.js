4bf6daf37d1819fe45d7dbc61f39203c38c39055
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-04-12 14:41:42 +0300

    added populate

--- Controllers/Cart.js
+++ Controllers/Cart.js
@@ -3,8 +3,11 @@ import Product from "../Models/Product.js";
 
 export const getCartItems = async (req, res) => {
   const userId = req.params.id;
+
   try {
-    let cart = await Cart.findOne({ userId });
+    let cart = await Cart.findOne({ userId })
+      .populate("items.productId")
+      .exec();
     if (cart && cart.items.length > 0) {
       res.send(cart);
     } else {
@@ -13,7 +16,7 @@ export const getCartItems = async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).send("Something went wrong");
-  }
+  }                                                                                                                                           
 };
 
 export const addCartItem = async (req, res) => {
