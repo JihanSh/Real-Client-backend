cb9b718e25ff559bd90c6ef779387c0430248019
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-04-13 09:33:54 +0300

    updated the cart

--- Controllers/Cart.js
+++ Controllers/Cart.js
@@ -1,88 +1,82 @@
 import Cart from "../Models/Cart.js";
-import Product from "../Models/productModel.js";
+import Product from "../Models/Product.js";
 
 export const getCartItems = async (req, res) => {
-    const userId = req.params.id;
-    try{
-        let cart = await Cart.findOne({ userId })
-        .populate("items.productId")
-        .exec();
-        if(cart && cart.items.length > 0){
-            res.send(cart);
-        }
-        else{
-            res.send(null);
-        }
+  const userId = req.params.id;
+
+  try {
+    let cart = await Cart.findOne({ userId })
+      .populate("items.productId")
+      .exec();
+    if (cart && cart.items.length > 0) {
+      res.send(cart);
+    } else {
+      res.send(null);
     }
-    catch(err) {
-        console.log(err);
-        res.status(500).send("Something went wrong");
-    }
-}
- 
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }                                                                                                                                           
+};
+
 export const addCartItem = async (req, res) => {
-    const userId = req.params.id;
-    const productId = req.body.productId;
-  
-    try {
-      let cart = await Cart.findOne({ userId });
-      let product = await Product.findOne({ _id: productId });
-  
-      if (!product) {
-        res.status(404).send("Product not found");
-        return;
-      }
-  
-      const price = product.price;
-      const name = product.title;
-  
-      if (!cart) {
-        const newCart = await Cart.create({
-          userId,
-          items: [{ productId }],
-          bill: price,
-        });
-        return res.status(201).send(newCart);
-      } else {
-        let itemIndex = cart.items.findIndex(
-          (p) => p.productId.toString() === productId.toString()
-        );
-  
-        if (itemIndex > -1) {
-          let productItem = cart.items[itemIndex];
-          productItem.quantity += 1;
-          cart.items[itemIndex] = productItem;
-        } else {
-          cart.items.push({ productId, name, price, quantity: 1 });
-        }
-  
-        cart.bill += price;
-        cart = await cart.save();
-        return res.status(201).send(cart);
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).send("Something went wrong");
+  const userId = req.params.id;
+  const productId = req.body.productId;
+  try {
+    let cart = await Cart.findOne({ userId });
+    let product = await Product.findOne({ _id: productId });
+    if (!product) {
+      res.status(404).send("Product not found");
+      return;
     }
-  };
+    let price;
+    if (product.discountPercentage > 0) {
+      price = product.discountedPrice;
+    } else {
+      price = product.price;
+    }
+    if (!cart) {
+      const newCart = await Cart.create({
+        userId,
+        items: [{ productId }],
+        bill: price,
+      });
+      return res.status(201).send(newCart);
+    } else {
+      let itemIndex = cart.items.findIndex(
+        (p) => p.productId.toString() === productId.toString()
+      );
+      if (itemIndex > -1) {
+        return res.send("Product already exists in cart.");
+      } else {
+        cart.items.push({ productId });
+      }
+      cart.bill += price;
+      cart = await cart.save();
+      return res.status(201).send(cart);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }
+};
 
 export const deleteItem = async (req, res) => {
   const userId = req.params.userId;
   const productId = req.params.productId;
-  console.log(productId)
   try {
-      let cart = await Cart.findOne({ userId });
-      let itemIndex = cart.items.findIndex(p => p.productId.toString() === productId);
-      if (itemIndex > -1) {
-          let productItem = cart.items[itemIndex];
-          if (!isNaN(productItem.price)) {
-              cart.bill -= productItem.price;
-          }
-          cart.items.splice(itemIndex, 1);
-      }
-      cart = await cart.save();
-      console.log('hello', cart);
-      return res.status(200).send(cart);
+    let cart = await Cart.findOne({ userId });
+    let product = await Product.findOne({ _id: productId });
+    let itemIndex = cart.items.findIndex(
+      (p) => p.productId.toString() === productId
+    );
+    if (itemIndex > -1) {
+      cart.items.splice(itemIndex, 1);
+      cart.bill -= product.getDiscountedPrice() || product.price;
+    }
+    cart = await cart.save();
+    // console.log("hello", cart);
+    return res.status(200).send(cart);
   } catch (err) {
     console.log(err);
     res.status(500).send("Something went wrong");
