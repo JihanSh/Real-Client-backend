839686fccdb416f60a990730eea087c0f9e40d7d
Jihan Shamas <jihan.shamas@gmail.com>
2023-04-24 23:42:23 +0300

    logout

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {register,login,getUserById,updateUser,updateRole,deleteUser} from "../Controllers/Auth.js";
+import {register,login,getUserById,updateUser,updateRole,deleteUser, logout} from "../Controllers/Auth.js";
 import { adminAuth } from "../middleware/Auth.js";
 
 
@@ -8,6 +8,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/user/:id", getUserById);
 router.put("/update", updateUser);
+router.post("/logout", logout);
 router.put("/updateRole", updateRole);
 router.delete("/delete", deleteUser);
 router.put("/updateRole",adminAuth, updateRole);
